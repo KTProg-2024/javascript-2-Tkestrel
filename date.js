0335edd69aa3e9eafc83b7dfef18bfe8cee7e2c9
Toby Hansen <>
2021-05-27 14:14:34 +0000

    Finished Assignment

--- date.js
+++ date.js
@@ -3,3 +3,21 @@ function getDate()
 	var dateString = new Date();
 	document.getElementById("date").innerHTML=dateString;
 }
+
+function light()
+{
+  document.getElementById("date").style.color="black";
+  document.getElementById("date").style.backgroundcolor="white";
+}
+
+function dark()
+{
+	document.getElementById("date").style.color="white";
+  document.getElementById("date").style.backgroundcolor="black";
+}
+
+function update()
+{
+  var dateString = new Date();
+	document.getElementById("date").innerHTML=dateString;
+}
